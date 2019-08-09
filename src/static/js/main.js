c06d397685fe243866019c93a1a2fe9f6eddaefb
Matt <maxwellmattryan@gmail.com>
2019-08-09 21:59:30 +0900

    Fix email form

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -18,36 +18,24 @@ $(document).ready(function() {
             event.preventDefault();
             nameStatusElm.append("<div>*please enter a name</div>");
             document.getElementsByName("name")[0].style.border = "1px solid red";
-        } else {
-            event.preventDefault();
-            document.getElementsByName("name")[0].style.border = "0px solid red";
         }
 
         if(email.length < 5 || !email.includes("@") || !email.includes(".")) {
             event.preventDefault();
             emailStatusElm.append("<div>*please enter a valid email</div>");
             document.getElementsByName("email")[0].style.border = "1px solid red";
-        } else {
-            event.preventDefault();
-            document.getElementsByName("email")[0].style.border = "none";
         }
 
         if(subject < 1) {
             event.preventDefault();
             subjectStatusElm.append("<div>*please enter a subject</div>");
             document.getElementsByName("subject")[0].style.border = "1px solid red";
-        } else {
-            event.preventDefault();
-            document.getElementsByName("subject")[0].style.border = "none";
-        }
+        } 
 
         if(message < 10) {
             event.preventDefault();
             messageStatusElm.append("<div>*please enter a message</div>");
             document.getElementsByName("message")[0].style.border = "1px solid red";
-        } else {
-            event.preventDefault();
-            document.getElementsByName("message")[0].style.border = "none";
         }
     })
 })
