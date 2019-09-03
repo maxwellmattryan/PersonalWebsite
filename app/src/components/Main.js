ff79f57a5745a30579920c317967ca595166a753
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-09-03 00:34:59 -0500

    Start project styling

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Project from "./Project.js"
 import OperationHome from "../img/operation-home.png";
+import DistortionPlugin from "../img/operation-home.png";
+import JLPTScrape from "../img/operation-home.png";
 import TechStack from "./TechStack.js";
 import BlogShowcase from "./BlogShowcase.js";
 import Footer from "./Footer.js";
@@ -10,9 +12,9 @@ import "../scss/main.scss";
 class Main extends Component {
     state = {
         projects : [
-            {name: "Operation H.O.M.E.", source: OperationHome},
-            {name: "Distortion VST Plugin", source: OperationHome},
-            {name: "JLPT Vocabulary Scraper", source: OperationHome},
+            {index: "project_0", layout: "0", source: OperationHome, title: "Operation H.O.M.E.", subtitle: "Global Game Jam 2019", description: "Defend the earth as aliens are attempting to invade and destroy it!", url: "https://github.com/maxwellmattryan/operation-home"},
+            {index: "project_1", layout: "1", source: DistortionPlugin, title: "Distortion VST Plugin", subtitle: "Music Production", description: "Distort audio in a gnarly way with control over the gain, drive, and mix.", url: "https://github.com/maxwellmattryan/basic-distortion-plugin"},
+            {index: "project_2", layout: "0", source: JLPTScrape, title: "JLPT Vocabulary Scraper", subtitle: "Japanese Language Proficiency Test", description: "Scrape jisho.org to find and export JLPT vocabulary words.", url: "https://github.com/maxwellmattryan/jisho-search"},
         ]
     }
     render() {
@@ -26,7 +28,7 @@ class Main extends Component {
                         <a className="read-more" href="about.html">Contact Me &rarr;</a>
                     </div>
                     <div className="scroll-container animated">
-                        <a className="readmore">&darr;</a>
+                        <a className="read-more" href="#project_0">Projects <br/> &darr;</a>
                     </div>
                 </div>
 
--- app/src/components/Project.js
+++ app/src/components/Project.js
@@ -6,17 +6,19 @@ class Project extends Component {
         const { projects } = this.props;
         const projectList = projects.map(project => {
             return(
-                <div className="project animated fade-in" key={ project.id }>
+                <div id={ project.index } className="project animated fade-in" key={ project.id }>
                     <section className="project-container">
                         <div className="project-inner-container">
-                            <div className="project-text-container">
-                                <img alt="project logo here"/>
-                                <p className="project-title">{ project.name }</p>
-                                <p className="project-description">Project description.</p>
-                                <a className="read-more">see project on github -></a>
-                            </div>
                             <div className="project-img-container">
-                                <img src={ project.source } alt="project image here"/>
+                                <a href={ project.url } className="read-more" target="_blank">
+                                    <img src={ project.source } alt={ project.title}/>
+                                </a>
+                            </div>
+                            <div className="project-text-container">
+                                <p className="project-title">{ project.title }</p>
+                                <p className="project-subtitle">{ project.subtitle }</p>
+                                <p className="project-description">{ project.description }</p>
+                                <a href={ project.url } className="read-more" target="_blank">View on GitHub &rarr;</a>
                             </div>
                         </div>
                     </section>
@@ -24,9 +26,9 @@ class Project extends Component {
             )
         })
         return(
-            <div className="projects">
+            <>
                 { projectList }
-            </div>
+            </>
         )
     }
 }
