20f439fe320a767a0b6430cd12e8c55d79263bf6
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-05-21 13:17:54 -0500

    Add basic frontend routing

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,17 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { PostComponent } from './components/post/post.component';
+import { CategoryComponent } from './components/category/category.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'admin/login', component: LoginComponent },
+  { path: 'blog', component: BlogComponent },
+  //{ path: 'blog/categories/:name', component: CategoryComponent },
+  //{ path: 'blog/posts/:id', component: PostComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    AdminComponent,
+    BlogComponent,
+    PostComponent,
+    CategoryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
