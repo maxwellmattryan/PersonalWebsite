4d68c2db5bf3b5a00bd32d658d7c0d820acc4939
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-06-04 12:10:51 -0500

    Add ordinals for reformatted date

--- angular-src/src/app/components/post/post.component.ts
+++ angular-src/src/app/components/post/post.component.ts
@@ -53,6 +53,18 @@ export class PostComponent implements OnInit {
 
     getFormattedDate(date: Date): string {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+
+        const nth = (day) => {
+            if(day > 3 && day < 21) return 'th'; 
+            
+            switch(day % 10) {
+                case 1: return 'st';
+                case 2: return 'nd';
+                case 3: return 'rd';
+                default: return 'th';
+            }
+        };
+
+        return months[date.getMonth()] + ' ' + date.getDate() + nth(date.getDate()) + ', ' + date.getFullYear();
     }
 }
