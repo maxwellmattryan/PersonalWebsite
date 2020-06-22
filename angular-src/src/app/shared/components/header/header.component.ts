34ecc7f1bed35cfd8f8efeecb8aebf1bd81b745a
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-06-22 13:17:10 -0500

    Add light experimentation with angular material custom theming

--- angular-src/src/app/shared/components/header/header.component.ts
+++ angular-src/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '@app/core/authentication';
 
 @Component({
     selector: 'app-header',
@@ -6,12 +9,39 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+    isAdmin: boolean = false;
 
-    constructor() { }
+    shouldDisplaySidebar: boolean = false;
+    shouldDisplayNavbar: boolean = true;
 
-    ngOnInit(): void { }
+    private previousPageYOffset: number = 0;
 
-    toggleSideMenu(): void {
-        console.log('TODO: Add side menu functionality.');
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+        this.onRouterEvents();
+    }
+
+    onRouterEvents(): void {
+        this.router.events.subscribe((data) => {
+            this.isAdmin = this.authService.isLoggedIn();
+        });
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll() {
+        this.checkPageOffset();
+    }
+
+    checkPageOffset(): void {
+        this.shouldDisplayNavbar = window.pageYOffset < this.previousPageYOffset;
+        this.previousPageYOffset = window.pageYOffset;
+    }
+
+    toggleSidebar(): void {
+        this.shouldDisplaySidebar = !this.shouldDisplaySidebar;
     }
 }
