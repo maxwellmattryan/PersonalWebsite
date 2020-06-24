92148ffc7d604c7455c812660be9fd7269e29db8
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-06-24 11:54:36 -0500

    Fix some blog view button layout stuff

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "database": "mongodb://localhost:27017/mattmaxwell",
+    "database": "mongodb+srv://maxwellmattryan:<password>@mattmaxwell-db-bp2u1.gcp.mongodb.net/test",
     "secret": "mysecret"
 };
