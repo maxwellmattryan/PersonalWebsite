3b6e7cfae9baa183000411241b09880be86e3c29
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-07-29 17:13:16 -0500

    Remove unnecessary and clean directory

--- project/FrontendCommands.scala
+++
@@ -1,7 +0,0 @@
-// Contains commands for running front end npm scripts
-object FrontendCommands {
-    val install: String = "npm install"
-    val serve: String = "npm run start"
-    val test: String = "npm run test"
-    val build: String = "npm run build"
-}
