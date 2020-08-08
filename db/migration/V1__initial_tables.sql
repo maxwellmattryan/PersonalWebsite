e15cf1f3630b4e55a8280c663c0b335b26c45d25
Matthew Maxwell <maxwellmattryan@gmail.com>
2020-08-08 13:53:35 -0500

    Add function / trigger for updated_at columns in db tables

--- db/migration/V1__initial_tables.sql
+++ db/migration/V1__initial_tables.sql
@@ -1,3 +1,13 @@
+-- Handle an 'updated_at' field for a table
+CREATE FUNCTION update_updated_at_column() RETURNS trigger
+        LANGUAGE plpgsql
+        AS $$
+    BEGIN
+        NEW.updated_at = now();
+        RETURN NEW;
+    END;
+$$;
+
 CREATE TABLE admin (
     admin_id SERIAL PRIMARY KEY,
 
@@ -29,12 +39,16 @@ CREATE TABLE blog_post (
     content TEXT NOT NULL,
     image_url TEXT NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now(),
 
     FOREIGN KEY (blog_author_id) REFERENCES blog_author(blog_author_id),
     FOREIGN KEY (blog_post_status_id) REFERENCES blog_post_status(blog_post_status_id)
 );
+
+CREATE TRIGGER blog_post_updated_at_modtime
+    BEFORE UPDATE ON blog_post
+    FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
 
 CREATE TABLE blog_topic (
     blog_topic_id SERIAL PRIMARY KEY,
@@ -42,9 +56,13 @@ CREATE TABLE blog_topic (
     name VARCHAR(50) NOT NULL UNIQUE,
     description TEXT NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
 );
+
+CREATE TRIGGER blog_topic_updated_at_modtime
+    BEFORE UPDATE ON blog_topic
+    FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
 
 CREATE TABLE blog_post_topic_mapping (
     blog_post_id INT NOT NULL,
@@ -70,11 +88,15 @@ CREATE TABLE profile (
     landing TEXT NOT NULL,
     about TEXT NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now(),
 
     FOREIGN KEY (profile_status_id) REFERENCES profile_status(profile_status_id)
 );
+
+CREATE TRIGGER profile_updated_at_modtime
+    BEFORE UPDATE ON profile
+    FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
 
 CREATE TABLE project (
     project_id SERIAL PRIMARY KEY,
@@ -85,9 +107,13 @@ CREATE TABLE project (
     image_url TEXT NOT NULL,
     external_url TEXT NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
 );
+
+CREATE TRIGGER project_updated_at_modtime
+    BEFORE UPDATE ON project
+    FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
 
 CREATE TABLE profile_project_mapping (
     profile_id INT NOT NULL,
