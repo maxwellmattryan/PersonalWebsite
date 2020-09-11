045e196d13f27d96d1bda9c7fd9a3a589a12c1cc
maxwellmattryan <maxwellmattryan@gmail.com>
2020-09-11 11:14:22 -0500

    Fix some markdown issues

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { MarkdownModule } from 'ngx-markdown';
+import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CoreModule } from '@app/core/core.module';
 import { SharedModule } from '@app/shared/shared.module';
+
+export function markedOptionsFactory(): MarkedOptions {
+    const renderer = new MarkedRenderer();
+    const linkRenderer = renderer.link;
+
+    renderer.link = (href, title, text) => {
+        const html = linkRenderer.call(renderer, href, title, text);
+        return html.replace(/^<a /, '<a role="link" target="_blank" rel="nofollow noopener noreferrer" ');
+    }
+
+    return {
+        renderer,
+        gfm: true,
+        breaks: false,
+        pedantic: false,
+        smartLists: true,
+        smartypants: false,
+    };
+}
 
 @NgModule({
     declarations: [AppComponent],
@@ -21,7 +40,13 @@ import { SharedModule } from '@app/shared/shared.module';
         CoreModule,
         FormsModule,
         HttpClientModule,
-        MarkdownModule.forRoot(),
+        MarkdownModule.forRoot({
+            loader: HttpClient,
+            markedOptions: {
+                provide: MarkedOptions,
+                useFactory: markedOptionsFactory
+            }
+        }),
         ReactiveFormsModule,
         SharedModule
     ],
