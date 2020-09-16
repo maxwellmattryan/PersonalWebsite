2f6a342964f350081b6f52559147d40a1398150f
maxwellmattryan <maxwellmattryan@gmail.com>
2020-09-16 10:31:46 -0500

    Fix some SQL data problems with profiles

--- data/sql/profile-data.sql
+++ data/sql/profile-data.sql
@@ -5,7 +5,7 @@ INSERT INTO profile(status_id, name, tagline, landing, about, image_url)
 VALUES (2,
        'Audio Development',
        'I make noises happen through code.',
-       'I''m a software engineer based in Austin, TX. I love to build and develop software as it relates to audio, such as effect and synthesis plugins, digital signal processing, video game audio, microchip, and more.',
+       'I''m a software engineer based in Austin, TX. I love to build and develop software as it relates to audio, such as effect and synthesis plugins, digital signal processing, video game audio, microchips, and more.',
        'I strive to develop clean and optimized audio software, which in and of itself is an vastly interesting problem domain. I find its constraints to be fun and humbling, especially when coupled with my background in instrumental percussion and music production. Modifying digital signals of sound is an interesting concept that I find worth exploring.',
        'assets/images/profiles/portrait.png');
 INSERT INTO profile(status_id, name, tagline, landing, about, image_url)
@@ -13,7 +13,7 @@ VALUES (2,
        'Game Development',
        'I write code and design audio for video games.',
        'I''m a software engineer based in Austin, TX. I enjoy developing games, whether it''s systems and mechanics or audio programming and technical sound design.',
-       'I strive to develop both immersive audio and clean and neatly architectured code for video games. This pertains to things such as systems, mechanics, mechanics, audio implementation, and more. I find the fact that each and every game is different is what makes the entire process so enjoyable and informative.',
+       'I strive to develop both immersive audio and clean and neatly architectured code for video games. This pertains to things such as systems, mechanics, audio implementation, and more. I find the fact that each and every game is different is what makes the entire process so enjoyable and informative.',
        'assets/images/profiles/portrait.png');
 INSERT INTO profile(status_id, name, tagline, landing, about, image_url)
 VALUES (1,
