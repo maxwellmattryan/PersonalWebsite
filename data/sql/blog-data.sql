f051fe4fc65235316fcc1dd2bab604b16738d46f
maxwellmattryan <maxwellmattryan@gmail.com>
2020-09-16 10:59:01 -0500

    Fix another SQL problem

--- data/sql/blog-data.sql
+++ data/sql/blog-data.sql
@@ -4,6 +4,13 @@ INSERT INTO blog_post_status(status) VALUES ('DRAFT');
 INSERT INTO blog_post_status(status) VALUES ('PUBLISHED');
 INSERT INTO blog_post_status(status) VALUES ('ARCHIVED');
 
+INSERT INTO blog_topic(name, description) VALUES ('Audio Development', 'Any and all things related to audio development and audio programming.');
+INSERT INTO blog_topic(name, description) VALUES ('Game Development', 'Any and all things related to game development.');
+INSERT INTO blog_topic(name, description) VALUES ('Digital Signal Processing', 'Any and all things related to digital signal processing.');
+INSERT INTO blog_topic(name, description) VALUES ('Software Architecture', 'Any and all things related to software architecture.');
+INSERT INTO blog_topic(name, description) VALUES ('Software Design', 'Any and all things related to software design.');
+INSERT INTO blog_topic(name, description) VALUES ('Software Engineering', 'Any and all things related to software engineering.');
+
 INSERT INTO blog_post(author_id, status_id, title, subtitle, preview, content, image_url)
 VALUES (1,
        2,
@@ -114,13 +121,6 @@ ngOnInit(): void {
 
 Sed adipiscing diam donec adipiscing tristique. Interdum velit euismod in pellentesque massa. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. Posuere urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Mattis molestie a iaculis at. Amet porttitor eget dolor morbi non. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Ut morbi tincidunt augue interdum velit. Vel quam elementum pulvinar etiam non quam. Vulputate odio ut enim blandit volutpat maecenas volutpat. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Convallis posuere morbi leo urna molestie at elementum eu. Condimentum mattis pellentesque id nibh tortor id aliquet lectus. Amet venenatis urna cursus eget nunc scelerisque viverra mauris.',
        'assets/images/projects/green-foot.png');
-
-INSERT INTO blog_topic(name, description) VALUES ('Audio Development', 'Any and all things related to audio development and audio programming.');
-INSERT INTO blog_topic(name, description) VALUES ('Game Development', 'Any and all things related to game development.');
-INSERT INTO blog_topic(name, description) VALUES ('Digital Signal Processing', 'Any and all things related to digital signal processing.');
-INSERT INTO blog_topic(name, description) VALUES ('Software Architecture', 'Any and all things related to software architecture.');
-INSERT INTO blog_topic(name, description) VALUES ('Software Design', 'Any and all things related to software design.');
-INSERT INTO blog_topic(name, description) VALUES ('Software Engineering', 'Any and all things related to software engineering.');
 
 INSERT INTO blog_post_topics_blog_topic(blog_post_id, blog_topic_id) VALUES (1, 1), (1, 4);
 INSERT INTO blog_post_topics_blog_topic(blog_post_id, blog_topic_id) VALUES (2, 1), (2, 2);
