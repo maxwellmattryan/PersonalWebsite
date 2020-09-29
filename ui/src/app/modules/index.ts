305b2f390bba61782110e027375a96a8c11669db
maxwellmattryan <maxwellmattryan@gmail.com>
2020-09-29 12:08:56 -0500

    Delete primeng entirely

--- ui/src/app/modules/index.ts
+++ ui/src/app/modules/index.ts
@@ -4,5 +4,4 @@ export { EditorModule } from './editor/editor.module';
 export { HomeModule } from './home/home.module';
 export { IconModule } from './icon/icon.module';
 export { MaterialModule } from './material/material.module';
-export { PrimeNgModule } from './prime-ng/prime-ng.module';
 export { ProjectModule } from './project/project.module';
