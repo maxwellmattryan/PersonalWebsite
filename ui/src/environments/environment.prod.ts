59168ec1136cdcbfffd021487044de1ffd187218
maxwellmattryan <maxwellmattryan@gmail.com>
2020-10-11 18:20:09 -0500

    Change root url in environment

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    ROOT_URL: 'https://api.mattmaxwell.dev',
+    ROOT_URL: 'https://mattmaxwell.dev',
     API_URL: 'https://api.mattmaxwell.dev/api'
 };
