64fb24457f9c62e9de7e55255d5e6a15ea2d9172
maxwellmattryan <maxwellmattryan@gmail.com>
2021-02-24 16:22:39 -0600

    Create better design for obfuscation service

--- ui/src/app/core/services/obfuscation.service.ts
+++ ui/src/app/core/services/obfuscation.service.ts
@@ -7,6 +7,16 @@ export class ObfuscationService {
     constructor() { }
 
     constructEmail(name: string, domain: string, extension: string = 'com'): string {
-        return `${name}@${domain}.${extension}`;
+        return `mailto:${name}-at-${domain}-dot-${extension}`;
+    }
+
+    openEmail(event: Event, name: string, domain: string, extension: string = 'com'): void {
+        const mailToUrl = `mailto:${name}@${domain}.${extension}`;
+        const elem: HTMLAnchorElement = (<HTMLAnchorElement>event.target);
+        elem.href = mailToUrl;
+
+        setTimeout(() => {
+            elem.href = this.constructEmail(name, domain, extension);
+        }, 100);
     }
 }
