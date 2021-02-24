4340487f7add33ef1b4a82ab3f07fdad6bf0a773
maxwellmattryan <maxwellmattryan@gmail.com>
2021-02-24 16:43:19 -0600

    Add defaults for email

--- ui/src/app/core/services/obfuscation.service.ts
+++ ui/src/app/core/services/obfuscation.service.ts
@@ -4,19 +4,38 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class ObfuscationService {
+    private name: string = 'maxwellmattryan';
+    private domain: string = 'gmail';
+    private extension: string = 'com';
+
+    private funkyChar: string = '-_-';
+
     constructor() { }
 
-    constructEmail(name: string, domain: string, extension: string = 'com'): string {
-        return `mailto:${name}-at-${domain}-dot-${extension}`;
+    constructEmail(): string {
+        return this.constructEmailWith(this.name, this.domain, this.extension);
     }
 
-    openEmail(event: Event, name: string, domain: string, extension: string = 'com'): void {
+    handleEmailClick(event: Event): void {
+        this.handleEmailClickWith(event, this.name, this.domain, this.extension);
+    }
+
+    constructEmailWith(name: string, domain: string, extension: string = 'com'): string {
+        return `${this.uglify(name)}${this.funkyChar}at${this.funkyChar}}${this.uglify(domain)}${this.funkyChar}dot${this.funkyChar}${extension}`;
+    }
+
+    handleEmailClickWith(event: Event, name: string, domain: string, extension: string = 'com'): void {
         const mailToUrl = `mailto:${name}@${domain}.${extension}`;
         const elem: HTMLAnchorElement = (<HTMLAnchorElement>event.target);
         elem.href = mailToUrl;
 
         setTimeout(() => {
-            elem.href = this.constructEmail(name, domain, extension);
+            elem.href = `mailto:${this.constructEmailWith(name, domain, extension)}`;
         }, 100);
+    }
+
+    private uglify(s: string): string {
+        const idx = Math.floor(Math.random() * s.length);
+        return s.slice(0, idx) + this.funkyChar + s.slice(idx, s.length);
     }
 }
