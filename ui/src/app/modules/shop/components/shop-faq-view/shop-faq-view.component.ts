ad4d45ba062eafb1e9db85709b47294e76dbcf17
maxwellmattryan <maxwellmattryan@gmail.com>
2021-02-24 12:10:50 -0600

    Add stylings to FAQ page

--- ui/src/app/modules/shop/components/shop-faq-view/shop-faq-view.component.ts
+++ ui/src/app/modules/shop/components/shop-faq-view/shop-faq-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-export type FaqCategory = 'documentation' | 'bugs';
+export type FaqCategory = 'compatibility' | 'documentation' | 'bugs';
 
 @Component({
     selector: 'ui-shop-faq-view',
@@ -14,38 +14,47 @@ export class ShopFaqViewComponent implements OnInit {
     private categoryShowClass: string = 'faq__category-info--show';
     private categoryHiddenClass: string = 'faq__category-info--hidden';
 
-    private categoryHidden: boolean = true;
-
     constructor() { }
 
     ngOnInit(): void { }
 
-    public toggleCategory(category: FaqCategory): void {
-        this.categoryHidden = !this.categoryHidden;
-
+    public toggleCategory(category: FaqCategory, event: Event): void {
         const id = `faq__${category}`;
-        this.modifyStyling(
-            id,
-            this.categoryHidden ? this.categoryHiddenClass : this.categoryShowClass,
-            this.categoryHidden ? this.categoryShowClass : this.categoryHiddenClass
+        const faqCategory = document.getElementById(id);
+        let isHidden = this.elementContainsStyles(faqCategory, 'category');
+
+        (<HTMLInputElement>event.target).innerHTML = isHidden ? '&#8722;' : '&#x2b;';
+
+        this.modifyStyles(
+            faqCategory,
+            isHidden ? this.categoryShowClass : this.categoryHiddenClass,
+            isHidden ? this.categoryHiddenClass : this.categoryShowClass
         );
     }
 
-    public toggleAnswer(elementId: string): void {
+    public toggleAnswer(elementId: string, event: Event): void {
         let faqAnswer = document.getElementById(elementId);
+        let isHidden = this.elementContainsStyles(faqAnswer, 'answer');
 
-        let isHidden = faqAnswer.classList.contains(this.answerHiddenClass) || !faqAnswer.classList.contains(this.answerShowClass);
-        this.modifyStyling(
-            elementId,
+        (<HTMLInputElement>event.target).innerHTML = isHidden ? '&#8722;' : '&#x2b;';
+
+        this.modifyStyles(
+            faqAnswer,
             isHidden ? this.answerShowClass : this.answerHiddenClass,
             isHidden ? this.answerHiddenClass : this.answerShowClass
         );
     }
 
-    private modifyStyling(elementId: string, addClass: string, removeClass: string): void {
-        let faqInfo = document.getElementById(elementId);
+    private elementContainsStyles(elem: HTMLElement, type: 'category' | 'answer'): boolean {
+        const isCategory = type == 'category';
+        const showClass = isCategory ? this.categoryShowClass : this.answerShowClass;
+        const hiddenClass = isCategory ? this.categoryHiddenClass : this.answerHiddenClass;
 
-        faqInfo.classList.remove(removeClass);
-        faqInfo.classList.add(addClass);
+        return elem.classList.contains(hiddenClass) || !elem.classList.contains(showClass);
+    }
+
+    private modifyStyles(elem: HTMLElement, addClass: string, removeClass: string): void {
+        elem.classList.remove(removeClass);
+        elem.classList.add(addClass);
     }
 }
