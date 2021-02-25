be2164ebc6178ee591dfe68e461baa0ffdb1d69a
maxwellmattryan <maxwellmattryan@gmail.com>
2021-02-25 10:14:55 -0600

    FIX: Change base url in environment

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    BASE_URL: 'https://mattmaxwell.tech',
-    API_URL: 'https://api.mattmaxwell.tech/api',
+    BASE_URL: 'https://mattmaxwell.dev',
+    API_URL: 'https://api.mattmaxwell.dev/api',
     STRIPE_PK: 'pk_live_51IKRehEOcFDOpmIp4rQ4qf9vTA4QYExh3dtkFmSzwdQrPhPkUVGvr5ApVLEG1N6rmsmQoMBCDDGDKD8xotgvsQTi00fThFcKC4'
 };
