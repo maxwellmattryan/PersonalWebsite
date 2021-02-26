8883e126082691e2af39093fe118a953310dacc1
maxwellmattryan <maxwellmattryan@gmail.com>
2021-02-26 16:47:09 -0600

    Disable register button

--- api/src/core/http/filters/exceptions-logger.filter.ts
+++ api/src/core/http/filters/exceptions-logger.filter.ts
@@ -2,6 +2,10 @@ import { ArgumentsHost } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 
 export class ExceptionsLoggerFilter extends BaseExceptionFilter {
+    constructor() {
+        super();
+    }
+
     catch(exception: unknown, host: ArgumentsHost) {
         console.log('Exception thrown: ', exception);
         super.catch(exception, host);
