365e033140c5966b0648682627a6035e2a5e2648
maxwellmattryan <maxwellmattryan@gmail.com>
2021-04-04 22:30:16 -0500

    Go git route for Heroku deployment

--- heroku.sh
+++ heroku.sh
@@ -9,9 +9,9 @@ convertsecs() {
 NODE_VERSION=15.10.0-alpine3.10
 
 API_PROJECT=mattmaxwell-api
-HEROKU_API_IMAGE="heroku_$API_PROJECT:$(date +%s)"
+HEROKU_API_IMAGE="${API_PROJECT}_$(date +%s)"
 UI_PROJECT=mattmaxwell-ui
-HEROKU_UI_IMAGE="heroku_$UI_PROJECT:$(date +%s)"
+HEROKU_UI_IMAGE="${UI_PROJECT}_$(date +%s)"
 DOMAIN=mattmaxwell.dev
 
 API_ACTION=false
@@ -32,16 +32,16 @@ done
 START=1
 if [ "$API_ACTION" = true ] && [ "$UI_ACTION" = true ]
 then
-    STEPS=12
+    STEPS=5
 else
     if [ "$API_ACTION" = false ] && [ "$UI_ACTION" = false ]
     then
         API_ACTION=true
         UI_ACTION=true
 
-        STEPS=12
+        STEPS=5
     else
-        STEPS=7
+        STEPS=3
     fi
 fi
 
@@ -61,34 +61,30 @@ else
     echo -e "\t[✔] Branch is set to \"main\""
 fi
 
+if ! git diff-index --quiet HEAD --; then
+    echo -e "\t[✘] No existing repo changes\n"
+    echo -e "To rid current changes (temporarily), please use:\n\n\tgit stash"
+
+    exit 1;
+else
+    echo -e "\t[✔] No existing repo changes\n"
+fi
+
 echo -e "[Success]: Pre-build check(s) passed!\n"
 
 if [ "$API_ACTION" = true ]
 then
     cd api/ || echo -e "\nERROR: API folder does not exist" | exit
 
-    echo -e "($(expr $START)/$STEPS) Building local API image ...\n"
-    docker build . --tag "$API_PROJECT"
-    echo -e "[Success]: Built local API image!\n"
-
-    echo -e "($(expr $START + 1)/$STEPS) Tagging local API image for Container Registry ..."
-    docker tag "$API_PROJECT" "$HEROKU_API_IMAGE"
+    echo -e "($(expr $START)/$STEPS) Tagging local API image for Container Registry ..."
+    git commit --allow-empty -m "BUILD: $HEROKU_API_IMAGE"
     echo -e "[Success]: Tagged local API image!\n"
 
-    echo -e "($(expr $START + 2)/$STEPS) Pushing local API image to Container Registry ...\n"
-    heroku container:push "$HEROKU_API_IMAGE" -a "$API_PROJECT"
+    echo -e "($(expr $START + 1)/$STEPS) Pushing local API image to Container Registry ...\n"
+    git subtree push --prefix api heroku master
     echo -e "[Success]: Pushed local API image!\n"
 
-    echo -e "($(expr $START + 3)/$STEPS) Deploying to Heroku API project ...\n"
-    heroku container:release "$HEROKU_API_IMAGE" -a "$API_PROJECT"
-    echo -e "[Success]: Deployed app!\n"
-
-    echo -e "($(expr $START + 4)/$STEPS) Removing API images from Docker...\n"
-    docker rmi "$API_PROJECT:latest"
-    docker rmi "$HEROKU_API_IMAGE:latest"
-    echo -e "[Success]: Removed API image(s)!\n"
-
-    START=7
+    START=4
 
     cd ../
 fi
@@ -97,41 +93,23 @@ if [ "$UI_ACTION" = true ]
 then
     cd ui/ || echo -e "\nERROR: UI folder does not exist" | exit
 
-    echo -e "($(expr $START)/$STEPS) Building local UI image ...\n"
-    docker build . --tag "$UI_PROJECT"
-    echo -e "[Success]: Built local UI image!\n"
+    echo -e "($(expr $START)/$STEPS) Tagging local UI image for Container Registry ..."
+    git commit --allow-empty -m "BUILD: $HEROKU_UI_IMAGE"
+    echo -e "[Success]: Tagged local API image!\n"
 
-    echo -e "($(expr $START + 1)/$STEPS) Tagging local UI image for Container Registry ..."
-    docker tag "$UI_PROJECT" "$HEROKU_UI_IMAGE"
-    echo -e "[Success]: Tagged local UI image!\n"
-
-    echo -e "($(expr $START + 2)/$STEPS) Pushing local UI image to Container Registry ...\n"
-    heroku container:push "$HEROKU_UI_IMAGE" -a "$UI_PROJECT"
+    echo -e "($(expr $START + 1)/$STEPS) Pushing local UI image to Container Registry ...\n"
+    git subtree push --prefix ui heroku master
     echo -e "[Success]: Pushed local UI image!\n"
 
-    echo -e "($(expr $START + 3)/$STEPS) Deploying to Heroku UI project ...\n"
-    heroku container:release "$HEROKU_UI_IMAGE" -a "$UI_PROJECT"
-    echo -e "[Success]: Deployed app!\n"
-
-    echo -e "($(expr $START + 4)/$STEPS) Removing UI images from Docker...\n"
-    docker rmi "$UI_PROJECT:latest"
-    docker rmi "$HEROKU_UI_IMAGE:latest"
-    echo -e "[Success]: Removed UI image(s)!\n"
-
-    if [ "$STEPS" = 12 ]
+    if [ "$STEPS" = 5 ]
     then
-        START=12
+        START=5
     else
-        START=7
+        START=4
     fi
 
     cd ../
 fi
-
-echo -e "($(expr $START)/$STEPS) Cleaning up dangling / unused images...\n"
-docker rmi "node:$NODE_VERSION"
-printf 'y\n' | docker image prune
-echo -e "[Success]: Cleaned up images!\n"
 
 end_time=$(date +%s)
 execution_time=$(expr $end_time - $start_time)
